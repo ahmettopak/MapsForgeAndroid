f722aa2a124101dbc7cd338948bd70739fe3409c
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2024-01-20 14:24:13 +0300

    Auto center add.

--- app/src/main/java/com/ahmet/mapsforgeapp/MainActivity.java
+++ app/src/main/java/com/ahmet/mapsforgeapp/MainActivity.java
@@ -1,46 +1,41 @@
 package com.ahmet.mapsforgeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 
 import android.content.Intent;
 
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 
-import org.mapsforge.core.graphics.Bitmap;
-import org.mapsforge.core.graphics.Paint;
-import org.mapsforge.core.graphics.Style;
+import com.ahmet.mapsforgeapp.databinding.ActivityMainBinding;
+
 import org.mapsforge.core.model.LatLong;
-import org.mapsforge.map.android.graphics.AndroidBitmap;
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
-import org.mapsforge.map.android.layers.MyLocationOverlay;
-import org.mapsforge.map.android.util.AndroidUtil;
-import org.mapsforge.map.android.view.MapView;
-import org.mapsforge.map.datastore.MapDataStore;
-import org.mapsforge.map.layer.Layers;
-import org.mapsforge.map.layer.cache.TileCache;
-import org.mapsforge.map.layer.overlay.Circle;
 import org.mapsforge.map.layer.overlay.Marker;
-import org.mapsforge.map.layer.renderer.TileRendererLayer;
-import org.mapsforge.map.reader.MapFile;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import org.mapsforge.map.rendertheme.InternalRenderTheme;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+    ActivityMainBinding binding;
 
     private static final int PICK_MAP_REQUEST = 1;
     private static final int AUTO_LOAD_MAP_REQUEST = 2;
@@ -50,51 +45,109 @@ public class MainActivity extends AppCompatActivity {
     double konyaLatitude = 37.8714;
     double konyaLongitude = 32.4846;
     Marker robotMarker;
+    private Timer fakeGpsTimer;
 
     MapViewController mapViewController;
+
+    boolean autoCenter = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         mapViewController = new MapViewController(this);
 
         AndroidGraphicFactory.createInstance(this.getApplication());
-        Button selectMapButton = findViewById(R.id.selectMapButton);
-        selectMapButton.setOnClickListener(view -> pickMapFile());
+
+        autoCenter = binding.autoCenterCheckBox.isChecked();
+
+        binding.selectMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickMapFile();
+            }
+        });
+
+        binding.gpsServiceCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    startFakeGpsTimer(1000);
+                }
+                else{
+                    stopFakeGpsTimer();
+                }
+            }
+        });
+
+        binding.autoCenterCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                autoCenter = b;
+            }
+        });
+
 
         mapViewController.setMapView(findViewById(R.id.mapView));
+        robotMarker =  mapViewController.createMarker(new LatLong(ankaraLatitude,ankaraLongitude) , getDrawable(android.R.drawable.ic_menu_mylocation) );
+        mapViewController.getMapView().setCenter(robotMarker.getLatLong());
+        mapViewController.getMapView().setZoomLevel((byte) 15);
+        mapViewController.addMarker(robotMarker);
 
-        robotMarker =  mapViewController.addMarker(new LatLong(ankaraLatitude,ankaraLongitude) , getDrawable(android.R.drawable.ic_menu_mylocation) );
 
-//        Timer timer = new Timer();
-//        timer.scheduleAtFixedRate(new TimerTask() {
-//            @Override
-//            public void run() {
-//                // Yeni bir konum oluşturun (örneğin, rastgele bir konum)
-//                LatLong newLocation = mapViewController.generateRandomLocation();
-//
-//                // İkona yeni konumu ata
-//                robotMarker.setLatLong(newLocation);
-//
-//                // Haritayı güncelle
-//                mapViewController.getMapView().postInvalidate();
-//            }
-//        }, 0, 3000);
+    }
 
+    private void startFakeGpsTimer(int period) {
+
+        fakeGpsTimer = new Timer();
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                // Zamanlayıcı görevi burada çalıştırılır
+                // Örneğin, UI güncellemesi yapmak için Handler kullanabilirsiniz
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mapViewController.updateMarkerLatLong(robotMarker,mapViewController.generateRandomLocation(new LatLong(ankaraLatitude,ankaraLongitude) , 0.02));
+
+                        if (autoCenter){
+                            mapViewController.getMapView().setCenter(robotMarker.getLatLong());
+                        }
+                        mapViewController.addMarker(robotMarker);
+
+                        binding.gpsLocationTextView.setText(String.format("%s%s", getString(R.string.gps_location), robotMarker.getLatLong().toString()));
+
+                        Log.d(TAG,"Lat Long Test Link: " + generateLatLongMapsLink(robotMarker.getLatLong().latitude , robotMarker.getLatLong().longitude));
+
+
+                    }
+                });
+            }
+        };
+
+        // Zamanlayıcıyı belirli bir süre sonra başlat (örneğin, 1000 milisaniye sonra, ardından her 1000 milisaniyede bir)
+        fakeGpsTimer.schedule(timerTask, 0, period);
+    }
+
+    private void stopFakeGpsTimer() {
+        fakeGpsTimer.cancel();
     }
     private void pickMapFile() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-       /// intent.setType("*/*"); // Tüm dosya türlerini seçmek için
-
-
-        intent.setType("application/*"); // Sadece belirli bir uzantıyı seçmek için
-
-        String[] mimeTypes = {"application/map"}; // .map uzantılı dosyaları belirtin
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-
+        intent.setType("*/*"); // Tüm dosya türlerini seçmek için
         startActivityForResult(Intent.createChooser(intent, "Harita Dosyasını Seç"), PICK_MAP_REQUEST);
+    }
+    @NonNull
+    private static String generateLatLongMapsLink(double latitude, double longitude) {
+        Uri.Builder builder = Uri.parse("https://www.latlong.net/c/").buildUpon();
+        builder.appendQueryParameter("lat", String.valueOf(latitude));
+        builder.appendQueryParameter("long", String.valueOf(longitude));
+        String url = builder.build().toString();
+        return url;
     }
 
     @Override
@@ -115,127 +168,20 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 mapViewController.addMapTile(fis);
+
+
+                //Todo fix marker z offset
+                mapViewController.addMarker(robotMarker);
+
             }
         }
 
     }
 
-
-
-//    private void openMap(Uri uri) {
-//
-//           try {
-//
-//               mapView.getMapScaleBar().setVisible(true);
-//               mapView.setBuiltInZoomControls(true);
-//
-//               Log.d("TAG", "openMap: " + uri);
-//               /*
-//                * To avoid redrawing all the tiles all the time, we need to set up a tile cache with an
-//                * utility method.
-//                */
-//               TileCache tileCache = AndroidUtil.createTileCache(this, "mapcache",
-//                       mapView.getModel().displayModel.getTileSize(), 1f,
-//                       mapView.getModel().frameBufferModel.getOverdrawFactor());
-//
-//               /*
-//                * Now we need to set up the process of displaying a map. A map can have several layers,
-//                * stacked on top of each other. A layer can be a map or some visual elements, such as
-//                * markers. Here we only show a map based on a mapsforge map file. For this we need a
-//                * TileRendererLayer. A TileRendererLayer needs a TileCache to hold the generated map
-//                * tiles, a map file from which the tiles are generated and Rendertheme that defines the
-//                * appearance of the map.
-//                */
-//               FileInputStream fis = (FileInputStream) getContentResolver().openInputStream(uri);
-//               MapDataStore mapDataStore = new MapFile(fis);
-//               TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore,
-//                       mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
-//               tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.DEFAULT);
-//
-//
-//
-//               /*
-//                * On its own a tileRendererLayer does not know where to display the map, so we need to
-//                * associate it with our mapView.
-//                */
-//               mapView.getLayerManager().getLayers().add(tileRendererLayer);
-//               mapView.getFpsCounter().setVisible(true);
-//
-//
-//               mapView.getLayerManager().getLayers().add(myLocationOverlay);
-//
-//               createlayerMark();
-//
-//
-//               LatLong targetLocation = new LatLong(39.9334, 32.8597);  // Ankara'nın koordinatları
-//
-//               mapView.setCenter(targetLocation);
-//
-//               Bitmap markerBitmap = AndroidGraphicFactory.convertToBitmap(getResources().getDrawable(R.drawable.ic_launcher_foreground));
-//               Marker marker = new Marker(targetLocation, markerBitmap, 0, 0);
-//
-//
-//
-//
-//               myLocationOverlay.setPosition(ankaraLatitude, ankaraLongitude, Float.valueOf(10));
-//
-//               mapView.setCenter(new LatLong(ankaraLatitude, ankaraLongitude));
-//
-//               mapView.getLayerManager().getLayers().add(marker);
-//
-//           }
-//           catch (Exception e){
-//               Log.d("TAG", "openMap: " + e);
-//           }
-//
-//    }
-//
-//    private static Paint getPaint(int color, int strokeWidth, Style style) {
-//        Paint paint = AndroidGraphicFactory.INSTANCE.createPaint();
-//        paint.setColor(color);
-//        paint.setStrokeWidth(strokeWidth);
-//        paint.setStyle(style);
-//        return paint;
-//    }
-//
-//    private void createlayerMark()
-//    {
-//        // marker to show at the location
-//        Bitmap bitmap = new AndroidBitmap(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_mylocation));
-//        Marker marker = new Marker(new LatLong(ankaraLatitude, ankaraLongitude), bitmap, 0, 0);
-//
-//
-//        // circle to show the location accuracy (optional)
-//        Circle circle = new Circle(new LatLong(ankaraLatitude, ankaraLongitude), 0,
-//                getPaint(AndroidGraphicFactory.INSTANCE.createColor(48, 0, 0, 255), 0, Style.FILL),
-//                getPaint(AndroidGraphicFactory.INSTANCE.createColor(160, 0, 0, 255), 2, Style.STROKE));
-//
-//        // create the overlay
-//
-//        myLocationOverlay = new MyLocationOverlay(marker, circle);
-//
-//        Layers layers = this.mapView.getLayerManager().getLayers();
-//        layers.add(this.myLocationOverlay);
-//
-//    }
-//    // Uri'den dosya yolu elde etme
-//    private String getPathFromUri(Uri uri) {
-//        String[] projection = {MediaStore.Images.Media.DATA};
-//        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-//        if (cursor != null) {
-//            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//            cursor.moveToFirst();
-//            String filePath = cursor.getString(columnIndex);
-//            cursor.close();
-//            return filePath;
-//        }
-//        return null;
-//    }
-
-
     @Override
     protected void onStart() {
         super.onStart();
+
     }
 
     @Override
@@ -243,6 +189,8 @@ public class MainActivity extends AppCompatActivity {
         try {
             mapViewController.getMapView().destroyAll();
             AndroidGraphicFactory.clearResourceMemoryCache();
+            AndroidGraphicFactory.clearResourceFileCache();
+
             super.onDestroy();
         } catch (Exception e) {
             Log.e("MainActivity", "Error in onDestroy", e);
--- app/src/main/java/com/ahmet/mapsforgeapp/MapViewController.java
+++ app/src/main/java/com/ahmet/mapsforgeapp/MapViewController.java
@@ -1,10 +1,13 @@
 package com.ahmet.mapsforgeapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 
@@ -19,6 +22,7 @@ import org.mapsforge.map.android.layers.MyLocationOverlay;
 import org.mapsforge.map.android.util.AndroidUtil;
 import org.mapsforge.map.android.view.MapView;
 import org.mapsforge.map.datastore.MapDataStore;
+import org.mapsforge.map.datastore.MultiMapDataStore;
 import org.mapsforge.map.layer.Layers;
 import org.mapsforge.map.layer.cache.TileCache;
 import org.mapsforge.map.layer.overlay.Circle;
@@ -26,50 +30,85 @@ import org.mapsforge.map.layer.overlay.Marker;
 import org.mapsforge.map.layer.renderer.TileRendererLayer;
 import org.mapsforge.map.reader.MapFile;
 import org.mapsforge.map.rendertheme.InternalRenderTheme;
+import org.mapsforge.map.util.MapViewProjection;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapViewController {
 
     private MapView mapView;
     private TileCache tileCache;
-
     private Context context;
+    List<Marker> markerList = new ArrayList<>();
+
+    MultiMapDataStore multiMapDataStore;
 
     public MapViewController(Context context){
         this.context = context;
+        multiMapDataStore = new MultiMapDataStore(MultiMapDataStore.DataPolicy.RETURN_ALL);
+
+
     }
-    public Marker addMarker(LatLong latLong, Drawable markerIcon) {
+    public Marker createMarker(LatLong latLong, Drawable markerIcon) {
         Marker marker = new Marker(latLong, AndroidGraphicFactory.convertToBitmap(markerIcon), 0, 0);
         mapView.getLayerManager().getLayers().add(marker);
+        markerList.add(marker);
         return marker;
+    }
+    public void updateMarkerLatLong(Marker marker , LatLong latLong) {
+
+        marker.setLatLong(latLong);
+
+    }
+
+    public void addMarker(Marker marker) {
+        if (mapView.getLayerManager().getLayers().contains(marker)){
+            mapView.getLayerManager().getLayers().remove(marker);
+            mapView.getLayerManager().getLayers().add(marker);
+
+        }
+        else{
+            mapView.getLayerManager().getLayers().add(marker);
+            markerList.add(marker);
+        }
     }
 
     public void addMapTile(FileInputStream fileInputStream) {
         try {
+            multiMapDataStore.addMapDataStore(new MapFile(fileInputStream) , false ,false);
 
-            setTileCache();
-
-            MapDataStore mapDataStore = new MapFile(fileInputStream);
-
-            TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore,
+            TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, multiMapDataStore,
                     mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
             tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.OSMARENDER);
 
             mapView.getLayerManager().getLayers().add(tileRendererLayer);
-            mapView.invalidate();
 
+           // mapView.invalidate();
+
+            tileRendererLayer.requestRedraw();
         } catch (Exception e) {
             Log.e("MapViewController", "Error while adding map tile: " + e.getMessage());
         }
     }
+
 
     public LatLong generateRandomLocation() {
         double lat = 37.7749 + (Math.random() - 0.5) * 0.1;
         double lon = -122.4194 + (Math.random() - 0.5) * 0.1;
         return new LatLong(lat, lon);
     }
-
+    public static LatLong generateRandomLocation(LatLong latLong) {
+        double lat = latLong.getLatitude() + (Math.random() - 0.5) * 0.1;
+        double lon = latLong.getLatitude() + (Math.random() - 0.5) * 0.1;
+        return new LatLong(lat, lon);
+    }
+    public static LatLong generateRandomLocation(LatLong latLong , double  tolerance) {
+        double lat = latLong.getLatitude() + (Math.random() - 0.5) * tolerance;
+        double lon = latLong.getLatitude() + (Math.random() - 0.5) * tolerance;
+        return new LatLong(lat, lon);
+    }
     public MapView getMapView() {
         return mapView;
     }
@@ -77,18 +116,28 @@ public class MapViewController {
     public void setMapView(@NonNull MapView mapView) {
         this.mapView = mapView;
         initMapView();
+        setTileCache();
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void initMapView() {
         mapView.setClickable(true);
         mapView.getMapScaleBar().setVisible(true);
         mapView.setBuiltInZoomControls(true);
         mapView.getFpsCounter().setVisible(true);
+        mapView.setZoomLevelMax((byte) 15);
+        mapView.setZoomLevelMin((byte) 5);
+
     }
 
     public void setTileCache() {
-        tileCache = AndroidUtil.createTileCache(context, "mapcache",
-                mapView.getModel().displayModel.getTileSize(), 2f,
-                mapView.getModel().frameBufferModel.getOverdrawFactor());
+        int tileSize = mapView.getModel().displayModel.getTileSize();
+        float scaleFactor = 1.0f;
+        double overdrawFactor = mapView.getModel().frameBufferModel.getOverdrawFactor();
+        int cacheSize = 1024; // Özel ayarlamalar yapabilirsiniz
+
+        tileCache = AndroidUtil.createTileCache(context, "mapcache", tileSize, scaleFactor, overdrawFactor);
     }
+
+
 }
